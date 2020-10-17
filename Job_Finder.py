705b0b7f41ca550dbfd76f4ddfd71dd6b15fe432
Tamizh <tamizhis5n@gmail.com>
2020-10-18 01:54:34 +0530

    removed csv

--- Job_Finder.py
+++ Job_Finder.py
@@ -2,7 +2,7 @@ from Indeed_in import indeed
 from FreshersWorld_IN import freshersWorld
 from Times_Job_In import times_jobs
 import pandas as pd
-def main(job,loc):
+def find_job(job,loc):
     a=indeed(job,loc)
     b=freshersWorld(job,loc)
     c=times_jobs(job,loc)
@@ -10,5 +10,5 @@ def main(job,loc):
     d.to_csv("Jobs_list.csv",index=False)
 
 if __name__=="__main__":
-  main("data scientist","chennai")
+  find_job("data scientist","chennai")
   #main(input("Job title?:\n").lower(),input("Job Location?\n").lower())
