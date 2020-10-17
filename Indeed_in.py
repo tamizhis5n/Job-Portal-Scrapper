32c9d49b91c6eea032cb7a74dc556b94fbbad280
Tamizh <tamizhis5n@gmail.com>
2020-10-17 20:39:30 +0530

    fixed requests import

--- Indeed_in.py
+++ Indeed_in.py
@@ -9,48 +9,49 @@ def striphtml(data):
 
 def indeed(job_title,location):
 
-  
-  X = job_title.replace(" ",'-')
-  Y = location.replace(" ",'-')
-  URL="https://www.indeed.co.in/"+X+"-jobs-in-"+Y
-  #print(URL)
-  soup = BeautifulSoup(urllib.request.urlopen(URL).read(), 'html.parser')
-  #print("32")
+  try:
+    X = job_title.replace(" ",'-')
+    Y = location.replace(" ",'-')
+    URL="https://www.indeed.co.in/"+X+"-jobs-in-"+Y
+    #print(URL)
+    soup = BeautifulSoup(urllib.request.urlopen(URL).read(), 'html.parser')
+    #print("32")
 
-  results = soup.find_all('div', attrs={'data-tn-component': 'organicJob'})
-  a=[]
-  b=[]
-  c = []
-  d = []
-  #print(results)
-  #print("32")
-  for x in results:
-      company = x.find('span', attrs={"class":"company"})
-      if company:
-          a.append(company.text.strip())
-      job = x.find('a', attrs={'data-tn-element': "jobTitle"})
-      if job:
-          b.append(job.text.strip())
-      link = x.find('a', href=True, attrs={'data-tn-element':'jobTitle'})
-      link = str('https://www.indeed.co.in' + link['href'])
-      print (link)
-      if link:
-          c.append(link)
-      jd = BeautifulSoup(urllib.request.urlopen(link).read(), 'html.parser')
-      jd_res = str(jd.find_all('div', attrs={'id': 'jobDescriptionText', 'class':'jobsearch-jobDescriptionText'})).replace("]", "")
-      #jd_res = [x.find('div') for x in jd_res]
-      zz=str(jd_res).replace("[", "")
-      d.append(striphtml(zz))
-  #print(a)
-  #print(b) 
-  #print(c)
-  #print(d)   
-  #print("@@@@@@@@@")    
-  indeed_df = pd.DataFrame(list(zip(b, a, c, d)),columns =['Job Title', 'Company', 'Link', 'Job_Description']) 
-  indeed_df['Location'] = location
-  indeed_df['Job_Rec'] = job_title
-  return(indeed_df)
-
+    results = soup.find_all('div', attrs={'data-tn-component': 'organicJob'})
+    a=[]
+    b=[]
+    c = []
+    d = []
+    #print(results)
+    #print("32")
+    for x in results:
+        company = x.find('span', attrs={"class":"company"})
+        if company:
+            a.append(company.text.strip())
+        job = x.find('a', attrs={'data-tn-element': "jobTitle"})
+        if job:
+            b.append(job.text.strip())
+        link = x.find('a', href=True, attrs={'data-tn-element':'jobTitle'})
+        link = str('https://www.indeed.co.in' + link['href'])
+        print (link)
+        if link:
+            c.append(link)
+        jd = BeautifulSoup(urllib.request.urlopen(link).read(), 'html.parser')
+        jd_res = str(jd.find_all('div', attrs={'id': 'jobDescriptionText', 'class':'jobsearch-jobDescriptionText'})).replace("]", "")
+        #jd_res = [x.find('div') for x in jd_res]
+        zz=str(jd_res).replace("[", "")
+        d.append(striphtml(zz))
+    #print(a)
+    #print(b) 
+    #print(c)
+    #print(d)   
+    #print("@@@@@@@@@")    
+    indeed_df = pd.DataFrame(list(zip(b, a, c, d)),columns =['Job Title', 'Company', 'Link', 'Job_Description']) 
+    indeed_df['Location'] = location
+    indeed_df['Job_Rec'] = job_title
+    return(indeed_df)
+  except:
+    pass
 
 if __name__=="__main__":
   #indeed("data scientist","chennai")
--- Times_Job_In.py
+++ Times_Job_In.py
@@ -1,52 +1,64 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.request
+import requests
 
+import re
+def striphtml(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
 
 def times_jobs(job_title,location):
-  try:
-    job_title_tj = job_title.replace(' ', '+')
-    location_tj = location.replace(' ', '+')
-    tj_url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=" + str(job_title_tj).replace(' ', '+') + "&txtLocation=" + str(location_tj)
-    r = requests.get(tj_url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    results = soup.find_all('ul', attrs={'class':'new-joblist'})  
-    count=0       
-    extracted_data=[]      
-    all_titles=[]  
-    all_links=[]    
-    all_company=[]  
-    job_description = []
-    for i in results:            
-            a=i.find_all('a')                                                                                           
-            companies=i.find_all('h3',attrs={'class':'joblist-comp-name'})    
-            for j in companies:
-                    
-                    company=str(j.text).lstrip()
-                    company=company.rstrip()
-                    all_company.append(company)            
-            count=0
-            for j in a:
-                    if(count%3==0):                                
-                            job_title=str(j.text).lstrip()
-                            all_titles.append(job_title) 
-                            link= str(j['href']).replace(" ", "%20")
-                            all_links.append(link)
-                            
-                    count+=1                 
-    for i in range(0,len(all_titles)):                                                    
-            extracted_data.append([all_titles[i],
-                                  all_company[i],
-                                  all_links[i]])
-    tj_df = pd.DataFrame(columns=['Job Title', 'Company', 'Link',], data=extracted_data)
-    for k in tj_df['Link'].values:
-        jd = BeautifulSoup(urllib.request.urlopen(str(k)).read(), 'html.parser')
-        jd_res = str(jd.find_all('div', attrs={'class':'jd-sec'})).replace("]", "")
-        job_description.append(str(jd_res).replace("[", ""))
-    tj_df['Company'] = tj_df['Company'].apply(lambda x: x.replace('(More Jobs)', ''))
-    tj_df['Job_Description'] = job_description
-    tj_df['Location'] = location
-    tj_df['Job_Rec'] = job_title
-    return(tj_df)
-  except:
-    pass
+    try:
+        job_title_tj = job_title.replace(' ', '+')
+        location_tj = location.replace(' ', '+')
+        tj_url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=" + str(job_title_tj).replace(' ', '+') + "&txtLocation=" + str(location_tj)
+        r = requests.get(tj_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        results = soup.find_all('ul', attrs={'class':'new-joblist'})  
+        count=0       
+        extracted_data=[]      
+        all_titles=[]  
+        all_links=[]    
+        all_company=[]  
+        job_description = []
+        for i in results:            
+                a=i.find_all('a')                                                                                           
+                companies=i.find_all('h3',attrs={'class':'joblist-comp-name'})    
+                for j in companies:
+                        
+                        company=str(j.text).lstrip()
+                        company=company.rstrip()
+                        all_company.append(company)            
+                count=0
+                for j in a:
+                        if(count%3==0):                                
+                                job_title=str(j.text).lstrip()
+                                all_titles.append(job_title) 
+                                link= str(j['href']).replace(" ", "%20")
+                                print(link)
+                                all_links.append(link)
+                                
+                        count+=1                 
+        for i in range(0,len(all_titles)):                                                    
+                extracted_data.append([all_titles[i],
+                                    all_company[i],
+                                    all_links[i]])
+        tj_df = pd.DataFrame(columns=['Job Title', 'Company', 'Link',], data=extracted_data)
+        for k in tj_df['Link'].values:
+            jd = BeautifulSoup(urllib.request.urlopen(str(k)).read(), 'html.parser')
+            jd_res = str(jd.find_all('div', attrs={'class':'jd-sec'})).replace("]", "")
+            job_description.append(str(jd_res).replace("[", ""))
+        tj_df['Company'] = tj_df['Company'].apply(lambda x: x.replace('(More Jobs)', ''))
+        tj_df['Job_Description'] = job_description
+        tj_df['Location'] = location
+        tj_df['Job_Rec'] = job_title
+        return(tj_df)
+    except:
+        pass
+
+
+if __name__=="__main__":
+  #times_jobs("data scientist","chennai")
+  df=times_jobs(input("Job title?:\n").lower(),input("Job Location?\n").lower())
+  df.to_csv("times_jobs.csv",index=False)
